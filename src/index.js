668f4bc2270d407f47b4464a6464056a24ddaee2
Vasja Hajnas <129309015+VasjaHajnas@users.noreply.github.com>
2023-09-08 09:57:58 +0300

    homework-JS11-finish13

--- src/index.js
+++ src/index.js
@@ -6,22 +6,17 @@ import {
 } from './js/ui.js';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 const searchForm = document.getElementById('search-form');
-
 const gallery = document.querySelector('.gallery');
 const lightbox = new SimpleLightbox('.gallery a');
-
 let currentPage = 1;
 let currentQuery = '';
 let initialLoad = true;
 let noResultsShown = false;
-
 async function searchImages(query) {
   try {
     const data = await fetchImages(query, currentPage);
     const { hits, totalHits } = data;
-
     if (hits.length === 0) {
       if (!noResultsShown) {
         showErrorMessage(
@@ -31,9 +26,7 @@ async function searchImages(query) {
       }
       return;
     }
-
     renderImages(hits);
-
     if (initialLoad) {
       showEndOfResultsMessage(totalHits);
       initialLoad = false;
@@ -45,24 +38,20 @@ async function searchImages(query) {
     );
   }
 }
-
 searchForm.addEventListener('submit', async e => {
   e.preventDefault();
-
   const searchInput = searchForm.querySelector('input[name="searchQuery"]');
   currentQuery = searchInput.value.trim();
-
   if (currentQuery === '') {
+    showErrorMessage('You have not entered a request');
     return;
   }
-
   currentPage = 1;
   gallery.innerHTML = '';
   initialLoad = true;
   noResultsShown = false;
   searchImages(currentQuery);
 });
-
 window.addEventListener('scroll', () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
   if (scrollTop + clientHeight >= scrollHeight - 100) {
@@ -72,5 +61,3 @@ window.addEventListener('scroll', () => {
     }
   }
 });
-
-searchImages(currentQuery);
